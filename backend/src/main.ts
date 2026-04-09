ba8ba8d2d00b77a835ca8ee437312dd271c9e42e
081114ysm <114277447+081114ysm@users.noreply.github.com>
2026-04-09 12:08:34 +0900

    로컬 경로를 배포된 경로로  수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
   const origins = getAllowedOrigins();
   app.enableCors({
     origin: origins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
